03adab288b2fef3f856e3d723047991791120832
Мирьян <miryan.asmanov@mail.ru>
2021-12-24 12:15:06 +0500

    Add project files.

---
+++ AuthOnWeb/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using AuthOnWeb.Entity;
+using AuthOnWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthOnWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        readonly UsersContext uc;
+        public HomeController(UsersContext usersContext)
+        {
+            uc = usersContext;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string login, string password)
+        {
+            if (uc.Users.Any(u => u.Login == login && u.Password == password))
+            {
+                return Content("Добро пожаловать, " + login);
+            }
+            else
+            {
+                return Content("Ошибка авторизации");
+            }
+        }
+        [HttpGet]
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Registration(string login, string password)
+        {
+            if (uc.Users.Any(u => u.Login == login))
+            {
+                return Content("Пользователь с такии логином уже существует");
+            }
+            else
+            {
+                uc.Add(new User() { Login = login, Password = password });
+                uc.SaveChanges();
+                return Content("Пользователь добавлен");
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AuthOnWeb/Entity/UsersContext.cs
@@ -0,0 +1,19 @@
+﻿using AuthOnWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthOnWeb.Entity
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public UsersContext(DbContextOptions<UsersContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
